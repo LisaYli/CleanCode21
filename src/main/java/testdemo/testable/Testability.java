b0864cd58d796d58b49cc0e6e0ae5bb78cbfe181
LisaYli <lisa.ylikangas@hotmail.com>
2021-12-08 14:20:52 +0100

    Initial commit

---
+++ src/main/java/testdemo/testable/Testability.java
@@ -0,0 +1,16 @@
+package testdemo.testable;
+
+import java.util.Random;
+
+public class Testability {
+
+    public static void main(String[] args) {
+        try {
+            Logger.log(LogLevel.WARNING, "Some warning - program is starting up or whatever");
+            MailSender.sendMail("some-invalid-email-address.com", "Program has started.");
+        } catch (IllegalArgumentException ex) {
+            Logger.log(LogLevel.ERROR, "An error occured: " + ex);
+        }
+    }
+}
+
