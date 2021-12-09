06b74810f6e8565da2b44fe8f04492b24745b362
LisaYli <lisa.ylikangas@hotmail.com>
2021-12-09 15:27:30 +0100

    added coins()-method in Player and impl in Game

--- src/main/java/trivia/Player.java
+++ src/main/java/trivia/Player.java
@@ -3,6 +3,7 @@ package trivia;
 public class Player {
     private final String name;
     private int place = 0;
+    private int coins = 0;
 
     Player(String name) {
         this.name = name;
@@ -11,18 +12,23 @@ public class Player {
     public String name() {
         return name;
     }
+
     public int place() {
         return place;
     }
 
     public void move(int roll) {
-      place += roll;
+        place += roll;
         if (place >= 12) {
             place -= 12;
         }
     }
+
+    public void addCoin() {
+        coins++;
+    }
+
+    public int coins() {
+        return coins;
+    }
 }
-/*
-        place += roll;
-        place %= 12;
- */
